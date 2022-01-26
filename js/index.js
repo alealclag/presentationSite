4f16a67c1b03dcbe238bdf6d6101c941a8dcbea0
alealclag <alealclag@alum.us.es>
2022-01-26 19:43:38 +0100

    Added animation to form button, changed js card to jquery

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
+// Form Button Animation
 document
   .querySelector(".sendMessageBtn")
   .addEventListener("click", function () {
     var sendCharm = new Audio("audio/formSent.mp3");
     sendCharm.play();
+    document.querySelector(".sendMessageBtn").classList.add("sentSuccess");
+    document.querySelector(".sendMessageBtn").setAttribute("disabled", "true");
+    document.querySelector(".sendFormText").classList.add("invisibility");
+    document
+      .querySelector(".sendMessageBtn i")
+      .classList.remove("invisibility");
   });
+
+// var i = 0;
+// while (i < 360) {
+//   setTimeout(function () {
+//     document.querySelector(".profilePic").style.transform =
+//       "rotate(" + i + "deg)";
+//   }, 1000);
+//   i++;
+// }
