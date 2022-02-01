dbc9ffd0169f247f4cacb05a4d443cf31022bc1b
alealclag <alealclag@alum.us.es>
2022-02-01 22:51:39 +0100

    Changed profile pic music dynamic to only clicks and added guides for it

--- js/index.js
+++ js/index.js
@@ -1,22 +1,63 @@
-// Form Button Animation
-document
-  .querySelector(".sendMessageBtn")
-  .addEventListener("click", function () {
-    var sendCharm = new Audio("audio/formSent.mp3");
-    sendCharm.play();
-    document.querySelector(".sendMessageBtn").classList.add("sentSuccess");
-    document.querySelector(".sendMessageBtn").setAttribute("disabled", "true");
-    document.querySelector(".sendFormText").classList.add("invisibility");
-    document
-      .querySelector(".sendMessageBtn i")
-      .classList.remove("invisibility");
-  });
+//Spin profile picture
 
-// var i = 0;
-// while (i < 360) {
-//   setTimeout(function () {
-//     document.querySelector(".profilePic").style.transform =
-//       "rotate(" + i + "deg)";
-//   }, 1000);
-//   i++;
-// }
+var profilePic = $(".profilePic");
+var spinProfilePic = false; //Flag for when mouse over profile pic
+var keepSpinProfilePic = false; //Flag for when clicked profile pic
+var profilePicMusic = new Audio("audio/profilePic_music.mp3");
+profilePicMusic.loop = true;
+var firstTip = true;
+
+profilePic.click(function () {
+  spinProfilePic = !spinProfilePic;
+  if (spinProfilePic) {
+    $("#playTip").fadeOut(); //Hide hover Tip
+
+    if (firstTip) {
+      firstTip = false;
+      $("#pauseTip").fadeIn(); //Hide button text
+    }
+  } else {
+    spinProfilePic = false;
+    $("#pauseTip").fadeOut(); //Hide button text
+  }
+});
+
+rotate(0);
+function rotate(degree) {
+  //If mouse is over or was clicked, play.
+  if (spinProfilePic || keepSpinProfilePic) {
+    // For webkit browsers: e.g. Chrome
+    profilePic.css({ WebkitTransform: "rotate(" + degree + "deg)" });
+    // For Mozilla browser: e.g. Firefox
+    profilePic.css({ "-moz-transform": "rotate(" + degree + "deg)" });
+
+    degree++;
+    profilePicMusic.play();
+  } else {
+    profilePicMusic.pause();
+  }
+  // Animate rotation with a recursive call
+  setTimeout(function () {
+    rotate(degree);
+  }, 30);
+}
+
+// Form Button Animation
+$(".sendMessageBtn").click(function () {
+  var sendCharm = new Audio("audio/formSent.mp3");
+  sendCharm.play();
+  $(".sendMessageBtn").attr("disabled", "true"); //Disable button
+  $(".sendMessageBtn").animate(
+    {
+      borderRadius: "100%",
+      width: "60px",
+      height: "60px",
+      padding: 0,
+    },
+    100,
+    "linear"
+  ); //Make button round
+
+  $(".sendFormText").css("display", "none"); //Hide button text
+  $(".sendMessageBtn i").removeClass("invisibility"); //Show check on button
+});
