8f7733869756b4193eb7de6ff2cdb94baf18ad8e
alealclag <alealclag@alum.us.es>
2022-02-12 15:09:58 +0100

    Minor fixes

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -32,5 +32,3 @@ app.post("/", function (req, res) {
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
-
-prueba
