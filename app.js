157b4537835370de36da1436f4afdcff2755d4ee
alealclag <alealclag@alum.us.es>
2022-02-24 23:28:55 +0100

    Implemented EJS for workExp, education and tech. Fixed embSys card on small devices

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 import https from "https";
+import * as cards from "./cards.js";
 
 /* jshint ignore:start */
 const __filename = fileURLToPath(import.meta.url);
@@ -15,9 +16,33 @@ const port = 8080;
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+var data = {
+  profileName: "Alejandro Alcántara Laguna",
+  profileTitle: "Developer & Computer Engineer",
+  profileDescription: "Still a rookie, so I'm looking for my best path",
+  workCards: [cards.cober, cards.nttdata],
+  eduCards: [cards.webdev, cards.us],
+  techCards: [
+    cards.node,
+    cards.react,
+    cards.bootstrap,
+    cards.jQuery,
+    cards.mongoDB,
+    cards.git,
+    cards.docker,
+    cards.dotNet,
+    cards.python,
+    cards.java,
+    cards.cplusplus,
+    cards.embSys,
+  ],
+};
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  // res.sendFile(__dirname + "/index.html");
+  res.render("index", data);
 });
 
 app.post("/", function (req, res) {
@@ -25,7 +50,8 @@ app.post("/", function (req, res) {
   let email = req.body.email;
   let message = req.body.message;
 
-  res.send("POST request to the homepage");
+  res.redirect("/");
+
   console.log(req.body);
 });
 
---
+++ cards.js
@@ -0,0 +1,127 @@
+//jshint esversion:6
+class card {
+  constructor(name, role, date, description, img) {
+    this.name = name;
+    this.role = role;
+    this.date = date;
+    this.description = description;
+    this.img = img;
+  }
+}
+
+export const cober = new card(
+  "Cober",
+  ".NET Developer",
+  "aug. 2021 - oct. 2021",
+  "The work done consisted in upgrading the client's application using .NET (with MVVM and asynchrony), as well as solving bugs and analyzing the client's necessities",
+  "images/cardPics/coberlogo.png"
+);
+export const nttdata = new card(
+  "NTTDATA",
+  "Intern .NET Developer",
+  "oct. 2020 - apr. 2021",
+  "The tasks developed during my internship on the company were fixing bugs, migrating functionalities and performing functional tests. The main technology used was .NET",
+  "images/cardPics/nttdatalogo.png"
+);
+
+export const webdev = new card(
+  "Udemy",
+  "Full-Stack Web Development Course",
+  "dec. 2021 - mar. 2022",
+  "55h full-stack web development course, including technologies like Node, React, Bootstrap, MongoDB & Express.js",
+  "images/cardPics/webdevlogo.png"
+);
+
+export const us = new card(
+  "University of Seville",
+  "Computer Science Degree",
+  "2017 - 2021",
+  "Worked on various fields, including web development, analysis and design of algorithms, embedded systems, AI and more",
+  "images/cardPics/uslogo.png"
+);
+
+export const node = new card(
+  "Node.js",
+  "",
+  "",
+  "",
+  "images/cardPics/techLogos/node.jpg"
+);
+
+export const react = new card(
+  "React",
+  "",
+  "",
+  "",
+  "images/cardPics/techLogos/react.jpg"
+);
+export const bootstrap = new card(
+  "Bootstrap",
+  "",
+  "",
+  "",
+  "images/cardPics/techLogos/bootstrap.jpg"
+);
+export const jQuery = new card(
+  "jQuery",
+  "",
+  "",
+  "",
+  "images/cardPics/techLogos/jQuery.jpg"
+);
+export const mongoDB = new card(
+  "MongoDB",
+  "",
+  "",
+  "",
+  "images/cardPics/techLogos/mongoDB.jpg"
+);
+export const git = new card(
+  "Git",
+  "",
+  "",
+  "",
+  "images/cardPics/techLogos/git.jpg"
+);
+export const docker = new card(
+  "Docker",
+  "",
+  "",
+  "",
+  "images/cardPics/techLogos/docker.jpg"
+);
+export const dotNet = new card(
+  ".Net (C#)",
+  "",
+  "",
+  "",
+  "images/cardPics/techLogos/net.jpg"
+);
+export const python = new card(
+  "Python",
+  "",
+  "",
+  "",
+  "images/cardPics/techLogos/python.jpg"
+);
+export const java = new card(
+  "Java",
+  "",
+  "",
+  "",
+  "images/cardPics/techLogos/java.jpg"
+);
+export const cplusplus = new card(
+  "C / C++",
+  "",
+  "",
+  "",
+  "images/cardPics/techLogos/c++.jpg"
+);
+export const embSys = new card(
+  "Embedded Systems",
+  "",
+  "",
+  "",
+  "images/cardPics/techLogos/embedded.jpg"
+);
