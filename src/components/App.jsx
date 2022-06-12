ebdb2f40c4d9d940c6c4a24689eb2fa89623689b
alealclag <alealclag@alum.us.es>
2022-06-12 18:55:44 +0200

    Implemented animations (mainly w/o hooks)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -36,132 +36,134 @@ function createLangCard(langCard) {
 
 export default function App() {
   return (
-    <div>
-      <React.StrictMode>
-        <Header key="Header" />
+    <React.StrictMode>
+      <Header key="Header" />
 
-        <PresentationSection
-          key="presentationSection"
-          pic="./images/prestentationPic.jpg"
-          playHint="Click on me for a comfy experience!"
-          pauseHint="And again if you prefer silence"
-          name="Alejandro Alcántara Laguna"
-          role="Web developer & Computer Engineer"
-          bio="Still a rookie, so I'm looking for my best path"
-        />
+      <PresentationSection
+        key="presentationSection"
+        pic="./images/prestentationPic.jpg"
+        playHint={
+          window.matchMedia("(max-width: 768px)").matches ||
+          "ontouchstart" in window ||
+          navigator.maxTouchPoints > 0 ||
+          navigator.msMaxTouchPoints > 0
+            ? "Tap on me for a comfy experience!"
+            : "Click on me for a comfy experience!"
+        }
+        pauseHint="And again if you prefer silence"
+        name="Alejandro Alcántara Laguna"
+        role="Web developer & Computer Engineer"
+        bio="Still a rookie, so I'm looking for my best path"
+      />
 
-        <div className="gradientUp" id="workGradient">
-          <h2 className="mx-auto">Work Experience</h2>
+      <div className="gradientUp" id="workGradient">
+        <h2 className="mx-auto">Work Experience</h2>
+      </div>
+      <section className="" id="workExpSection">
+        <div className="container">
+          <img src="./images/bgPics/work.png" className="bgPic" />
+
+          <div className="row justify-content-around sectionRow">
+            {cards.workEdu.map((card) =>
+              card.isEduCard ? null : createCard(card)
+            )}
+          </div>
         </div>
-        <section className="" id="workExpSection">
-          <div className="container">
-            <img src="./images/bgPics/work.png" className="bgPic" />
+      </section>
+      <div className="gradientDown" id="eduGradient"></div>
 
-            <div className="row justify-content-around sectionRow">
-              {cards.workEdu.map((card) =>
-                card.isEduCard ? null : createCard(card)
-              )}
-            </div>
+      <section className="" id="EduSection">
+        <div className="container">
+          <h2 className="mx-auto">Education</h2>
+          <img src="./images/bgPics/education.png" className="bgPic eduBgPic" />
+
+          <div className="row justify-content-around sectionRow">
+            {cards.workEdu.map((card) =>
+              card.isEduCard ? createCard(card) : null
+            )}
           </div>
-        </section>
-        <div className="gradientDown" id="eduGradient"></div>
+        </div>
+      </section>
 
-        <section className="" id="EduSection">
-          <div className="container">
-            <h2 className="mx-auto">Education</h2>
-            <img
-              src="./images/bgPics/education.png"
-              className="bgPic eduBgPic"
-            />
+      <div className="gradientUp" id="knowGradient"></div>
+      <section className="" id="KnowledgeSection">
+        <div className="container">
+          <h2 className="mx-auto">Knowledges</h2>
+          <div className="row justify-content-center">
+            <div className="col-xxl-5 col-xl-6 col-lg-8 col-md-10 col-sm-11 knowledgeBlock">
+              <h3 className="knowSubBlockTitle">Technologies</h3>
+              <div className="knowledgeSubBlock shadow-lg" id="techKnow">
+                <div className="container">
+                  <img
+                    src="./images/bgPics/tech.png"
+                    className="bgPic knowBgPic"
+                  />
 
-            <div className="row justify-content-around sectionRow">
-              {cards.workEdu.map((card) =>
-                card.isEduCard ? createCard(card) : null
-              )}
-            </div>
-          </div>
-        </section>
-
-        <div className="gradientUp" id="knowGradient"></div>
-        <section className="" id="KnowledgeSection">
-          <div className="container">
-            <h2 className="mx-auto">Knowledges</h2>
-            <div className="row justify-content-center">
-              <div className="col-xxl-5 col-xl-6 col-lg-8 col-md-10 col-sm-11 knowledgeBlock">
-                <h3 className="knowSubBlockTitle">Technologies</h3>
-                <div className="knowledgeSubBlock shadow-lg" id="techKnow">
-                  <div className="container">
-                    <img
-                      src="./images/bgPics/tech.png"
-                      className="bgPic knowBgPic"
-                    />
-
-                    <div className="row gy-4 justify-content-around">
-                      {cards.techCards.map((techCard) =>
-                        createTechCard(techCard)
-                      )}
-                    </div>
+                  <div className="row gy-4 justify-content-around">
+                    {cards.techCards.map((techCard) =>
+                      createTechCard(techCard)
+                    )}
                   </div>
                 </div>
               </div>
+            </div>
 
-              <div className="col-xxl-5 col-xl-6 col-lg-8 col-md-10 col-sm-11 knowledgeBlock">
-                <h3 className="knowSubBlockTitle">
-                  Languages (spoken, not for coding)
-                </h3>
-                <div className="knowledgeSubBlock shadow-lg" id="langKnow">
-                  <div className="container">
-                    <img
-                      src="./images/bgPics/lang.png"
-                      className="bgPic knowBgPic"
-                    />
-                    <h4 className="levelTitle levelTitle">Native</h4>
-                    <div className="row">
-                      {cards.langCards.map((langCard) =>
-                        langCard.level == 1 ? createLangCard(langCard) : null
-                      )}
-                    </div>
+            <div className="col-xxl-5 col-xl-6 col-lg-8 col-md-10 col-sm-11 knowledgeBlock">
+              <h3 className="knowSubBlockTitle">
+                Languages (spoken, not for coding)
+              </h3>
+              <div className="knowledgeSubBlock shadow-lg" id="langKnow">
+                <div className="container">
+                  <img
+                    src="./images/bgPics/lang.png"
+                    className="bgPic knowBgPic"
+                  />
+                  <h4 className="levelTitle levelTitle">Native</h4>
+                  <div className="row">
+                    {cards.langCards.map((langCard) =>
+                      langCard.level == 1 ? createLangCard(langCard) : null
+                    )}
+                  </div>
 
-                    <h4 className="levelTitle leftLangBlock">Advanced</h4>
-                    <div className="row justify-content-end">
-                      {cards.langCards.map((langCard) =>
-                        langCard.level == 2 ? createLangCard(langCard) : null
-                      )}
-                    </div>
+                  <h4 className="levelTitle leftLangBlock">Advanced</h4>
+                  <div className="row justify-content-end">
+                    {cards.langCards.map((langCard) =>
+                      langCard.level == 2 ? createLangCard(langCard) : null
+                    )}
+                  </div>
 
-                    <h4 className="levelTitle">Intermediate</h4>
-                    <div className="row">
-                      {cards.langCards.map((langCard) =>
-                        langCard.level == 3 ? createLangCard(langCard) : null
-                      )}
-                    </div>
+                  <h4 className="levelTitle">Intermediate</h4>
+                  <div className="row">
+                    {cards.langCards.map((langCard) =>
+                      langCard.level == 3 ? createLangCard(langCard) : null
+                    )}
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-        <div className="gradientDown" id="contactGradient"></div>
+        </div>
+      </section>
+      <div className="gradientDown" id="contactGradient"></div>
 
-        <section className="" id="contactSection">
-          <h2 className="mx-auto">Contact Me</h2>
+      <section className="" id="contactSection">
+        <h2 className="mx-auto">Contact Me</h2>
 
-          <div className="container">
-            <img src="./images/bgPics/mail.png" className="bgPic mailPic" />
+        <div className="container">
+          <img src="./images/bgPics/mail.png" className="bgPic mailPic" />
 
-            <div className="row justify-content-center">
-              <ContactForm key="contactForm" />
-            </div>
+          <div className="row justify-content-center">
+            <ContactForm key="contactForm" />
           </div>
-        </section>
+        </div>
+      </section>
 
-        <Footer
-          key="footer"
-          linkedInProfile="https://www.linkedin.com/in/alejandro-alcantara-laguna"
-          gitHubProfile="https://github.com/alealclag"
-          repoLink="https://github.com/alealclag/presentationSite"
-        />
-      </React.StrictMode>
-    </div>
+      <Footer
+        key="footer"
+        linkedInProfile="https://www.linkedin.com/in/alejandro-alcantara-laguna"
+        gitHubProfile="https://github.com/alealclag"
+        repoLink="https://github.com/alealclag/presentationSite"
+      />
+    </React.StrictMode>
   );
 }
--- src/components/presentationSection.jsx
+++ src/components/presentationSection.jsx
@@ -1,5 +1,46 @@
 import React from "react";
 
+let spinProfilePic = false; //Flag for when mouse over profile pic
+let profilePicMusic = new Audio("audio/profilePic_music.mp3");
+profilePicMusic.loop = true;
+let firstTip = true;
+
+function rotate(degree) {
+  //If mouse is over or was clicked, play.
+  if (spinProfilePic) {
+    // For webkit browsers: e.g. Chrome
+    $(".profilePic").css({ WebkitTransform: "rotate(" + degree + "deg)" });
+    // For Mozilla browser: e.g. Firefox
+    $(".profilePic").css({ "-moz-transform": "rotate(" + degree + "deg)" });
+
+    degree++;
+    profilePicMusic.play();
+  } else {
+    profilePicMusic.pause();
+  }
+  // Animate rotation with a recursive call
+  setTimeout(function () {
+    rotate(degree);
+  }, 30);
+}
+
+rotate(0);
+
+function rotatePlay() {
+  spinProfilePic = !spinProfilePic;
+  if (spinProfilePic) {
+    $("#playTip").fadeOut(); //Hide hover Tip
+
+    if (firstTip) {
+      firstTip = false;
+      $("#pauseTip").fadeIn(); //Hide button text
+    }
+  } else {
+    spinProfilePic = false;
+    $("#pauseTip").fadeOut(); //Hide button text
+  }
+}
+
 export default function presentationSection(properties) {
   return (
     <section className="" id="presentationSection">
@@ -8,6 +49,7 @@ export default function presentationSection(properties) {
           className="profilePic shadow-lg"
           src={properties.pic}
           alt="My profile picture"
+          onClick={rotatePlay}
         />
         <div className="profilePicInstruction" id="playTip">
           <span id="playTipText">{properties.playHint}</span>
